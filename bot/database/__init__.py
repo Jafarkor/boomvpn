cc9c6c50ff8aafeb07a554cefcded426c02f35e8
Jafar Korchagin <korjafar@gmail.com>
2026-02-17 16:19:25 +0300

    fix bug

--- bot/database/__init__.py
+++ bot/database/__init__.py
@@ -1,4 +1,4 @@
+from bot.database.manager import create_pool, close_pool, get_pool
 from bot.database.init_db import create_tables
-from bot.database.manager import db
 
-__all__ = ["db", "create_tables"]
+__all__ = ["create_pool", "close_pool", "get_pool", "create_tables"]
