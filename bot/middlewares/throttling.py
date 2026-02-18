3ba282e820e9911ea49c2d29957449e8b5d4fb0d
Jafar Korchagin <korjafar@gmail.com>
2026-02-18 09:15:42 +0300

    fix bug

--- bot/middlewares/throttling.py
+++ bot/middlewares/throttling.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 
 logger = logging.getLogger(__name__)
 
-RATE_LIMIT = 0.5  # секунд между запросами
+RATE_LIMIT = 0.4  # секунд между запросами
 
 
 class ThrottlingMiddleware(BaseMiddleware):
