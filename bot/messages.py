36e95309b434e8c9ad02f3d3a737bf6a7af95180
Jafar Korchagin <korjafar@gmail.com>
2026-02-22 14:11:18 +0300

    minor UI updates

--- bot/messages.py
+++ bot/messages.py
@@ -120,9 +120,9 @@ def settings_text(sub: dict) -> str:
 def sub_url_text(url: str) -> str:
     return (
         "<i>Нажми, чтобы скопировать.</i>\n\n"
-        f"{'<tg-emoji emoji-id="5382261056078881010"></tg-emoji>' * 11}\n\n"
+        f"{'<tg-emoji emoji-id="5382261056078881010"></tg-emoji>'}\n\n"
         f"<code>{url}</code>\n"
-        f"{'<tg-emoji emoji-id="5382261056078881010"></tg-emoji>' * 11}\n\n"
+        f"{'<tg-emoji emoji-id="5382261056078881010"></tg-emoji>'}\n\n"
         "<i>Если VPN перестал работать — просто открой меню и обнови ссылку.</i>"
     )
 
